7aaf31d8bf87d4c68ae2be217fb24cc0b71258e7
Zhang Minghan <zmh13054618081@dingtalk.com>
2022-11-13 23:01:23 +0800

    2.1.3
    1. WebSocket Security Support
    2. Gunicorn Server Support

--- static/js/websocket_url.js
+++ static/js/websocket_url.js
@@ -3,10 +3,15 @@ class websocket {
         this.socket = undefined;
         this.message_window = message_window;
         this.url = `ws://${window.location.host}/${ url }/${ token }/`;
+        this.wss_url = `wss://${window.location.host}/${ url }/${ token }/`;
         this.create();
     }
     create() {
-        this.socket = new WebSocket(this.url);
+        try{
+            this.socket = new WebSocket(this.url);
+        }catch (DOMException) {
+            this.socket = new WebSocket(this.wss_url);
+        }
         let dom = this.message_window;
         this.socket.onopen = function (e) {
             alerts(["alert-success", "message-data"], `服务器 ${window.location.host} 连接成功!`, dom);
