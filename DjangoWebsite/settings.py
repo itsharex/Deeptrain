83e3d9370cb105683b199eae3d47d0710a57cad4
Zhang Minghan <zmh13054618081@dingtalk.com>
2023-01-12 18:35:04 +0800

    2.17.3

--- DjangoWebsite/settings.py
+++ DjangoWebsite/settings.py
@@ -231,7 +231,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, "/static/")
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, '/static/media/')
 
 # Haystack & Blog Settings
 HAYSTACK_CONNECTIONS = {
